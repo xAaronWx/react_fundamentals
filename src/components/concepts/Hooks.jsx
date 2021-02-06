a35d1a4160443684fc22f50eaeaa729ac7fe2013
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2021-02-05 19:25:26 -0500

    Click Counter

--- src/components/concepts/Hooks.jsx
+++ src/components/concepts/Hooks.jsx
@@ -28,4 +28,53 @@ const Hooks = () => {
   );
 };
 
-export default Hooks;
+const Hooks2 = () => {
+  const [results, queryNum, setQueryNum] = useNumHook("");
+  const [clicks, setClicks] = useClicks(0);
+
+  return (
+    <div className="main">
+      <div className="mainDiv">
+        <h3>Enter a number below to see a number fact</h3>
+        <input
+          value={queryNum}
+          onChange={(e) => setQueryNum(e.target.value)}
+          placeholder="Enter a number"
+        />
+        {results ? <h2>{results}</h2> : <di></di>}
+        <button onClick={() => setClicks(clicks + 1)}>
+          Click to update document
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const useNumHook = (num) => {
+  const [queryNum, setQueryNum] = useState(num);
+  const [results, setResults] = useState("");
+
+  useEffect(() => {
+    if (queryNum !== "") {
+      fetch(`http://numbersapi.com/${queryNum}`)
+        .then((res) => res.text())
+        .then((json) => {
+          setResults(json);
+          console.log(json);
+        });
+    }
+  }, [queryNum]);
+
+  return [results, queryNum, setQueryNum];
+};
+
+const useClicks = (iniCount) => {
+  const [clicks, setClicks] = useState(iniCount);
+
+  useEffect(() => {
+    document.title = `You have clicked ${clicks} times`;
+  }, [clicks]);
+  return [clicks, setClicks];
+};
+
+export default Hooks2;
